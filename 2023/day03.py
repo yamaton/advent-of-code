35d8774c83415ef17df058bddb8a6d9fc45e8219
Yamato Matsuoka <yamaton@gmail.com>
2023-12-03 19:08:19 -0500

    Update day03

--- 2023/day03.py
+++ 2023/day03.py
@@ -3,25 +3,26 @@ Usage:
   python day03.py input.txt
 """
 from pydantic import BaseModel
-from typing import Set
+from typing import Set, Tuple
 
 
 class Coordinate(BaseModel):
     x: int
     y: int
 
-    # def __repr__(self):
-    #     return f"C({self.x}, {self.y})"
+    def __repr__(self):
+        return f"C({self.x}, {self.y})"
 
     class Config:
         frozen = True
+
 
 class Part(BaseModel):
     number: int
     coordinates: frozenset[Coordinate]
 
-    # def __repr__(self):
-    #     return f"Part({self.number}, {self.coordinates})"
+    def __repr__(self):
+        return f"Part({self.number}, {self.coordinates})"
 
     class Config:
         frozen = True
@@ -31,16 +32,16 @@ class Symbol(BaseModel):
     symbol: str
     coord: Coordinate
 
-    # def __repr__(self):
-    #     return f"Symbol({self.symbol}, {self.coord})"
+    def __repr__(self):
+        return f"Symbol({self.symbol}, {self.coord})"
 
     class Config:
         frozen = True
 
 
-def neighbors(
-    coord: Coordinate, xmax: int, ymax: int
-) -> frozenset[Coordinate]:
+def neighbors(coord: Coordinate, grid_shape: Tuple[int, int]) -> frozenset[Coordinate]:
+    xmax, ymax = grid_shape
+
     def _f():
         x = coord.x
         y = coord.y
@@ -51,6 +52,7 @@ def neighbors(
                 if _j < 0 or _j >= ymax:
                     continue
                 yield Coordinate(x=_i, y=_j)
+
     return frozenset(_f())
 
 
@@ -61,6 +63,7 @@ def locate_symbols(grid: list[str]) -> frozenset[Symbol]:
                 if c != "." and not c.isdigit():
                     coord = Coordinate(x=i, y=j)
                     yield Symbol(symbol=c, coord=coord)
+
     return frozenset(_f())
 
 
@@ -75,26 +78,39 @@ def locate_parts(grid: list[str]) -> frozenset[Part]:
                     str_list.append(c)
                 else:
                     if coords:
-                        yield Part(number=int("".join(str_list)), coordinates=frozenset(coords))
+                        yield Part(
+                            number=int("".join(str_list)), coordinates=frozenset(coords)
+                        )
                         coords = set()
                         str_list = []
             if coords:
                 yield Part(number=int("".join(str_list)), coordinates=frozenset(coords))
+
     return frozenset(_f())
 
 
 def day03a(grid: list[str]) -> int:
     symbols = locate_symbols(grid)
     parts = locate_parts(grid)
-    xmax = len(grid)
-    ymax = len(grid[0])
-    territory = {loc for sym in symbols for loc in neighbors(sym.coord, xmax, ymax)}
+    shape = len(grid), len(grid[0])
+    territory = {loc for sym in symbols for loc in neighbors(sym.coord, shape)}  # type: ignore
     for p in parts:
         x = p.coordinates & territory
-        if x:
-            print(x)
-    selected_parts = {p for p in parts if any(p.coordinates & territory)}
+    selected_parts = {p for p in parts if any(p.coordinates & territory)}  # type: ignore
     return sum(p.number for p in selected_parts)
+
+
+def day03b(grid: list[str]) -> int:
+    symbols = locate_symbols(grid)
+    parts = locate_parts(grid)
+    shape = len(grid), len(grid[0])
+    power = 0
+    for sym in symbols:
+        territory = {loc for loc in neighbors(sym.coord, shape)}  # type: ignore
+        xs = [p for p in parts if any(p.coordinates & territory)]  # type: ignore
+        if len(xs) == 2:
+            power += xs[0].number * xs[1].number
+    return power
 
 
 if __name__ == "__main__":
@@ -103,3 +119,4 @@ if __name__ == "__main__":
     with open(sys.argv[1], "r") as f:
         grid = [line.strip() for line in f.readlines()]
     print(day03a(grid))
+    print(day03b(grid))
